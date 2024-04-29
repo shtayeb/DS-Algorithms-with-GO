ef8e13fee48057fa304f41cd4eba953110598cb8
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-04-29 14:21:23 +0430

    ready for arraylist

--- list/arraylist.go
+++ list/arraylist.go
@@ -1,8 +1,30 @@
 package list
 
+// watch the primeagen video on arryalist
 type ArrayList struct {
+	data   []int
+	length int
 }
 
-// func (a ArrayList) New() ArrayList {
-// 	return ArrayList{}
-// }
+func NewArrayList() ArrayList {
+	return ArrayList{
+		data:   []int{},
+		length: 0,
+	}
+}
+
+func (a ArrayList) push(value int) {
+
+}
+
+func (a ArrayList) pop() int {
+	return 0
+}
+
+func (a ArrayList) get(index int) int {
+	return 0
+}
+
+func (a ArrayList) delete(index int) int {
+	return 0
+}
